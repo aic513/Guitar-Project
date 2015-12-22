d027adedcfd99301838d3d4e07b2ed08f4fa0c72
Anton Popov <a@jokerinteractive.ru>
2015-12-22 07:37:57 +0300

    добавление формы в backend + куча мелких поправок

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -1,3 +1,42 @@
-/**
- * Created by A on 22.12.2015.
- */
+(function(){
+
+  var link = document.querySelector('.info-right');
+  var popup = document.querySelector('.write-us');
+  var close = popup.querySelector('.write-us-close');
+  var form = popup.querySelector('.contact-form-on-button');
+  var login = form.querySelector('#login-field');
+  var password = form.querySelector('#email-field');
+  var storage = localStorage.getItem('login');
+
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    popup.classList.add('write-us-show');
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+  }, false);
+
+  close.addEventListener('click', function(event) {
+    event.preventDefault();
+    popup.classList.remove('write-us-show');
+  }, false);
+
+  form.addEventListener('submit', function(event) {
+    if (!login.value || !password.value) {
+      event.preventDefault();
+      popup.classList.add('login-popup-error');
+    } else {
+      localStorage.setItem('login', login.value);
+    }
+  }, false);
+
+  window.addEventListener('keydown', function(event) {
+    if (event.keyCode == 27 && popup.classList.contains('write-us-show')) {
+      popup.classList.remove('write-us-show');
+    }
+  }, false);
+
+})();
