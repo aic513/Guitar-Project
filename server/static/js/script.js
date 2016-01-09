d2960b6c9001d0fca9890d7b8ef50403c0e76463
aic513 <aic513@mail.ru>
2016-01-09 17:41:58 +0300

    Сделал код формы обратного звонка,добавил анимацию на сайт

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -1,18 +1,14 @@
+// Функция вывода формы обратной связи на главной картинке
+
 (function() {
 
     var link = document.querySelector('.info-right');
     var popup = document.querySelector('.write-us');
     var close = popup.querySelector('.write-us-close');
-   
+
     link.addEventListener('click', function(event) {
         event.preventDefault();
         popup.classList.add('write-us-show');
-        if (storage) {
-            login.value = storage;
-            email.focus();
-        } else {
-            login.focus();
-        }
     }, false);
 
     close.addEventListener('click', function(event) {
@@ -23,6 +19,34 @@
     window.addEventListener('keydown', function(event) {
         if (event.keyCode == 27 && popup.classList.contains('write-us-show')) {
             popup.classList.remove('write-us-show');
+        }
+    }, false);
+
+
+})();
+
+
+// Функция вывода формы обратного звока
+
+(function() {
+
+    var tel = document.querySelector('.contact-right');
+    var popup_tel = document.querySelector('.tel-write-us');
+    var close = popup_tel.querySelector('.call-us-close');
+
+    tel.addEventListener('click', function(event) {
+        event.preventDefault();
+        popup_tel.classList.add('contact-right-show');
+    }, false);
+
+    close.addEventListener('click', function(event) {
+        event.preventDefault();
+        popup_tel.classList.remove('contact-right-show');
+    }, false);
+
+    window.addEventListener('keydown', function(event) {
+        if (event.keyCode == 27 && popup.classList.contains('contact-right-show')) {
+            popup_tel.classList.remove('contact-right-show');
         }
     }, false);
 
